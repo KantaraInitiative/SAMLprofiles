1ca42ee654badfa551d715f7478e40946cfbc080
samlprofiles.bot@netcup8.hoerbe.at <samlprofiles.bot@netcup8.hoerbe.at>
2017-01-20 08:10:58 +0100

    make script

--- make_html.sh
+++ make_html.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-cd /root/SAMLprofiles.master
-git pull
+# script to update contents from master branch into gh-pages
 
-asciidoctor -o ../SAMLprofiles.gh-pages/SAMLeGovImplementaton_2_0.html edit/egov-impl/main.adoc
-#asciidoctor-pdf -o ../SAMLprofiles.gh-pages/SAMLeGovImplementaton_2_0.pdf edit/egov-impl/main.adoc
+REPOMASTER="$HOME/SAMLprofiles.master"
+REPOGHPAGES="$HOME/SAMLprofiles.gh-pages"
 
+# .master and .ghpages are separatly checked-out repos, only linked by remote origin
+
+cd $REPOGHPAGES
+git pull # prevent merge conflict
+
+cd $REPOMASTER
+git pull # get uodated contents
+asciidoctor -o $REPOGHPAGES/SAMLeGovImplementaton_2_0.html edit/egov-impl/main.adoc
+#asciidoctor-pdf -o $REPOGHPAGES/SAMLeGovImplementaton_2_0.pdf edit/egov-impl/main.adoc
+
+cd $REPOGHPAGES
 git add -A
 git commit -m 'generated'
 git push
-
-
